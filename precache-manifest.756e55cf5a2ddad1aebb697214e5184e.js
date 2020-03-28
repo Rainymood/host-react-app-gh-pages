3f11e0277107da65bdac3b74fe863d5e588525a0
Jan Meppe <janmeppe@hotmail.com>
2020-03-28 15:15:26 +0100

    Updates

---
+++ precache-manifest.756e55cf5a2ddad1aebb697214e5184e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1a14c9abbbdf622ef72dfbc7baa6f2",
+    "url": "/host-react-app-gh-pages/index.html"
+  },
+  {
+    "revision": "0d79b5c270785835bf91",
+    "url": "/host-react-app-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e9d825590a02ce4436e",
+    "url": "/host-react-app-gh-pages/static/js/2.6d9acd4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/host-react-app-gh-pages/static/js/2.6d9acd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d79b5c270785835bf91",
+    "url": "/host-react-app-gh-pages/static/js/main.df78d81c.chunk.js"
+  },
+  {
+    "revision": "dbb97331b4da4a6c184d",
+    "url": "/host-react-app-gh-pages/static/js/runtime-main.a4bc6e1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/host-react-app-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.001910e5781e21b5ede9b20f085a08f1.js"
+  "/host-react-app-gh-pages/precache-manifest.756e55cf5a2ddad1aebb697214e5184e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/host-react-app-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
